fcaf9978b7f0c02ca3fd0d6e87396a46c269fa70
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-08-02 10:10:52 +1000

    Enable ATOM feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,8 @@ DEFAULT_LANG = "en"
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 FEED_ALL_RSS = "feeds/all.rss.xml"
-CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+TAG_FEED_ATOM = "feeds/tag-{slug}.atom.xml"
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
--- publishconf.py
+++ publishconf.py
@@ -13,6 +13,7 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+TAG_FEED_ATOM = "feeds/tag-{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
